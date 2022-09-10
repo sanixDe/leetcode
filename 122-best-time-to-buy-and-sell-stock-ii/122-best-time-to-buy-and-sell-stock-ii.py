1ea37e4425d8099eb802e36569b0421d19a9ae1c
sanixDe <105024947+sanixDe@users.noreply.github.com>
2022-09-10 01:51:16 -0500

    Time: 141 ms (11.03%), Space: 15.1 MB (69.34%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        valley = prices[0]
+        peak = prices[0]
+        profit = 0
+        i = 0
+        while i < len(prices)-1:
+            while i+1 < len(prices) and prices[i] >= prices[i+1]:
+                i += 1
+            valley = prices[i]
+            while i+1< len(prices) and prices[i] <= prices[i+1]:
+                i+= 1
+            peak = prices[i]
+            
+            profit += peak-valley
+        return profit
+                
