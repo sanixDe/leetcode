cc3dd0a7f3d4ff863c693d5ffd0edd53c1bbd9e2
sanixDe <105024947+sanixDe@users.noreply.github.com>
2022-10-10 19:40:44 -0500

    Time: 413 ms (19.87%), Space: 14 MB (57.03%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -5,13 +5,9 @@ class Solution:
         res = 0
         
         for r in range(len(s)):
-            
-            if s[r] in freq:
-                freq[s[r]] += 1
-            else:
-                freq[s[r]] = 1
-            
-            while r-l+1 - max(freq.values()) > k:
+            freq[s[r]] = 1 + freq.get(s[r], 0)
+
+            if r-l+1 - max(freq.values()) > k:
                 freq[s[l]] -= 1
                 l += 1
                 
