653a529ee9d3f9b8cc50b4b6fa74a7397b1417df
sanixDe <105024947+sanixDe@users.noreply.github.com>
2022-10-23 22:09:27 -0500

    Time: 54 ms (48.02%), Space: 13.9 MB (14.12%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,28 +1,17 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         
-        if len(palindrome) == 1:
+        n = len(palindrome)
+        
+        if n <= 1:
             return ""
         
-        if len(palindrome) % 2 == 0:
-            
-            for i in range(len(palindrome)):
-                if ord(palindrome[i]) > 97:
-                    palindrome = palindrome[:i] + "a" + palindrome[i+1:]
-                    return palindrome
-                elif i == len(palindrome) -1:
-                    palindrome = palindrome[:i] + chr(ord(palindrome[i])+1) + palindrome[i+1:]
-                    return palindrome
-        else:
-
-            for i in range(len(palindrome)):
-                if i != len(palindrome) // 2:
-
-                    if ord(palindrome[i]) > 97:
-                        palindrome = palindrome[:i] + "a" + palindrome[i+1:]
-                        return palindrome
-                    elif i == len(palindrome) -1:
-                        palindrome = palindrome[:i] + chr(ord(palindrome[i])+1) + palindrome[i+1:]
-                        return palindrome
-
+        for i in range(n):
+            if palindrome[i] != 'a':
+                if n % 2 != 0 and n // 2 == i:
+                    continue
+                return palindrome[:i] + 'a' + palindrome[i+1:]
+        
+        return palindrome[:n-1] + 'b'
+    
                     
