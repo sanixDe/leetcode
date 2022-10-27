5409e81f424d8a335175cce22f4168140b59a69e
sanixDe <105024947+sanixDe@users.noreply.github.com>
2022-10-26 20:53:34 -0500

    Time: 216 ms (27.40%), Space: 24.3 MB (13.27%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -9,27 +9,47 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         
-        # finding the middle element
+        '''
+        Three steps
+            -> Finding the middle element in linked list
+            -> After that reverse the second linkedlist (middle - end)
+            -> Merge two linkedlists
         
-        slow, fast = head, head
+        '''
+        
+        # STEP 1 finding the middle node with fast and slow pointer
+        
+        slow = fast = head
+        
         while fast and fast.next:
+            
             slow = slow.next
             fast = fast.next.next
-
+        
+        # STEP 2 reversing the second linkedlist
+        
         cur = slow
         pre = None
+        
         while cur:
             temp = cur.next
             cur.next = pre
             pre = cur
             cur = temp
+            
+            # pre, cur, cur.next = pre, cur.next, cur
+            
+        # Step 3 merging the linkedlist
         
-        first, second = head, pre
+        first = head
+        second = pre
+        
         while second.next:
+            
             first.next, first = second, first.next
             second.next, second = first, second.next
             
-            
+        
         
         '''
         Time Complexity = O(N)
