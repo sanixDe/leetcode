ad256c836f7903df2def42141c5b12dd83ec7f8b
sanixDe <105024947+sanixDe@users.noreply.github.com>
2022-10-28 00:58:40 -0500

    Time: 90 ms (85.76%), Space: 18.8 MB (68.44%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+#         p_val = p.val
+#         q_val = q.val
+        
+#         root_val = root.val
+        
+#         if root_val > p_val and root_val > q_val:
+#             return self.lowestCommonAncestor(root.right, p, q)
+#         if root_val < p_val and root_val < q_val:
+#             return self.lowestCommonAncestor(root.left, p, q)
+#         else:
+#             return root
+
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
