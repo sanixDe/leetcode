3795d0401286cd9a4befd3f8bbd81c8bafb51618
sanixDe <105024947+sanixDe@users.noreply.github.com>
2022-10-31 22:59:07 -0500

    Time: 245 ms (36.38%), Space: 17.3 MB (17.11%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,33 +1,37 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         
+        visited = set()
         adj = {i:[] for i in range(n)}
+        
         for i in edges:
             adj[i[0]].append(i[1])
             adj[i[1]].append(i[0])
-        visited = set()
-        
+            
+            
         def dfs(node):
+            
             if node in visited:
-                return 
+                return
             
             visited.add(node)
             
-            for x in adj[node]:
-                dfs(x)
-          
+            for c in adj[node]:
+                dfs(c)
+                
+                
         
-        ans = 0
+        res = 0
         for i in range(n):
             if i not in visited:
-                print(i)
                 dfs(i)
-                ans += 1
-        
-        return ans
-        
-        
-        '''
-        Time complexity = O(N + V) Since we are traversing through every node and edge
-        Space Complexity = O(N)        
-        '''
+                res += 1
+                
+        return res
+    
+    
+    '''
+    Time complexity = O(n+v)
+
+    
+    '''
