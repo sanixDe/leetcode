320b3f40f2aba6986fcf02fafb559e9437321fff
sanixDe <105024947+sanixDe@users.noreply.github.com>
2023-04-04 17:15:09 -0500

    Time: 117 ms, Memory: 14.3 MB - LeetHub

---
+++ 0the-earliest-moment-when-everyone-become-friends/0the-earliest-moment-when-everyone-become-friends.py
@@ -0,0 +1,22 @@
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        
+        logs = sorted(logs, key = lambda x:x[0])
+        print(logs)
+        
+        graph = collections.defaultdict(set)
+        print(graph)
+        
+        for i in range(n):
+            graph[i] = {i}
+            
+        for ts, n1, n2 in logs:
+            graph[n1] = graph[n1].union(graph[n2])
+            
+            for x in graph[n1]:
+                graph[x] = graph[n1]
+            
+            if len(graph[n1]) == n:
+                return ts
+        
+        return -1
